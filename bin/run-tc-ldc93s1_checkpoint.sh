dbb5b498d1665c4a5bec817b016630530d6c9494
Reuben Morais <reuben.morais@gmail.com>
2018-10-22 11:34:16 -0300

    Remove initialize_from_frozen_model flag and support code

--- bin/run-tc-ldc93s1_checkpoint.sh
+++ bin/run-tc-ldc93s1_checkpoint.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 set -xe
+set pipefail
 
 ldc93s1_dir="./data/ldc93s1-tc"
 ldc93s1_csv="${ldc93s1_dir}/ldc93s1.csv"
+
+epoch_count=$1
 
 if [ ! -f "${ldc93s1_dir}/ldc93s1.csv" ]; then
     echo "Downloading and preprocessing LDC93S1 example data, saving in ${ldc93s1_dir}."
@@ -14,10 +17,14 @@ python -u DeepSpeech.py \
   --train_files ${ldc93s1_csv} --train_batch_size 1 \
   --dev_files ${ldc93s1_csv} --dev_batch_size 1 \
   --test_files ${ldc93s1_csv} --test_batch_size 1 \
-  --n_hidden 494 --epoch 1 --random_seed 4567 --default_stddev 0.046875 \
+  --n_hidden 494 --epoch -1 --random_seed 4567 --default_stddev 0.046875 \
   --max_to_keep 1 --checkpoint_dir '/tmp/ckpt' --checkpoint_secs 0 \
   --learning_rate 0.001 --dropout_rate 0.05  --export_dir '/tmp/train' \
   --nouse_seq_length --decoder_library_path '/tmp/ds/libctc_decoder_with_kenlm.so' \
   --lm_binary_path 'data/smoke_test/vocab.pruned.lm' \
-  --lm_trie_path 'data/smoke_test/vocab.trie' \
-  --initialize_from_frozen_model '/tmp/frozen_model.pb'
+  --lm_trie_path 'data/smoke_test/vocab.trie' | grep -q "Training of Epoch $epoch_count"
+
+if [ $? -ne 0 ]; then
+  echo "Did not resume training from checkpoint"
+  exit 1;
+fi
--- bin/run-tc-ldc93s1_new.sh
+++ bin/run-tc-ldc93s1_new.sh
@@ -5,6 +5,8 @@ set -xe
 ldc93s1_dir="./data/ldc93s1-tc"
 ldc93s1_csv="${ldc93s1_dir}/ldc93s1.csv"
 
+epoch_count=$1
+
 if [ ! -f "${ldc93s1_dir}/ldc93s1.csv" ]; then
     echo "Downloading and preprocessing LDC93S1 example data, saving in ${ldc93s1_dir}."
     python -u bin/import_ldc93s1.py ${ldc93s1_dir}
@@ -14,8 +16,9 @@ python -u DeepSpeech.py \
   --train_files ${ldc93s1_csv} --train_batch_size 1 \
   --dev_files ${ldc93s1_csv} --dev_batch_size 1 \
   --test_files ${ldc93s1_csv} --test_batch_size 1 \
-  --n_hidden 494 --epoch 105 --random_seed 4567 --default_stddev 0.046875 \
-  --max_to_keep 1 --checkpoint_dir '/tmp/ckpt' --checkpoint_secs 0 \
+  --n_hidden 494 --epoch $epoch_count --random_seed 4567 \
+  --default_stddev 0.046875 --max_to_keep 1 \
+  --checkpoint_dir '/tmp/ckpt' --checkpoint_secs 0 \
   --learning_rate 0.001 --dropout_rate 0.05  --export_dir '/tmp/train' \
   --decoder_library_path '/tmp/ds/libctc_decoder_with_kenlm.so' \
   --lm_binary_path 'data/smoke_test/vocab.pruned.lm' \
